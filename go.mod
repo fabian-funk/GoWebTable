528fe52ae882532bda898ccde4954cc0215963d8
fabian-funk <funky.fabian@googlemail.com>
2024-04-24 09:41:45 +0000

    Sqlite3

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module main
-
 go 1.19
+
+require github.com/mattn/go-sqlite3 v1.14.22
+
+
--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "log"
-    "databasw/sql"
-    _ "github.com/mattn/go-sqlite3"
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"net/http"
 )
 
 func main() {
-    
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-    })
-  
-    log.Println("Server started on: http://localhost:80")
-    http.ListenAndServe(":80", nil)
+
+	db, _ := sql.Open("sqlite3", "stocks.db")
+	rows, _ := db.Query("SELECT * FROM STOCKS")
+
+	var stock, date, price string
+
+	for rows.Next() {
+		_ = rows.Scan(&stock, &date, &price)
+		log.Printf("Result: %s: %s: %s", stock, date, price)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	log.Println("Server started on: http://localhost:80")
+	http.ListenAndServe(":80", nil)
 }
